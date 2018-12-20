7361b722dc5926af8190b013259359cab9e61bde
Roman Pelikh <pelikh.roman@gmail.com>
2018-12-20 01:02:18 -0400

    Updates

---
+++ precache-manifest.02d4043e06904672b3310c2a4e1e54a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c524ea7e592d78235f8477f1dee1abb",
+    "url": "/Manage_Contacts_React_context/static/media/1.3c524ea7.jfif"
+  },
+  {
+    "revision": "aee82dd580e42c509f1a",
+    "url": "/Manage_Contacts_React_context/static/js/runtime~main.aee82dd5.js"
+  },
+  {
+    "revision": "562f8f46643f6d7cb140",
+    "url": "/Manage_Contacts_React_context/static/js/main.562f8f46.chunk.js"
+  },
+  {
+    "revision": "eec59ad0daf753c7dccb",
+    "url": "/Manage_Contacts_React_context/static/js/1.eec59ad0.chunk.js"
+  },
+  {
+    "revision": "562f8f46643f6d7cb140",
+    "url": "/Manage_Contacts_React_context/static/css/main.6f767a65.chunk.css"
+  },
+  {
+    "revision": "eec59ad0daf753c7dccb",
+    "url": "/Manage_Contacts_React_context/static/css/1.b8aaf86e.chunk.css"
+  },
+  {
+    "revision": "ac6ac59c5245ead2e336ca4d3a75c2a9",
+    "url": "/Manage_Contacts_React_context/index.html"
+  }
+];
